066b58f4e4ecd3e4be32144496862c09431330ec
sw961174314 <961174314@qq.com>
2024-06-26 11:16:47 +0800

    2024.6.26 增加乘车人表，生成持久层代码，增加乘车人类型枚举

---
+++ train-member/src/main/java/com/java/train/mapper/PassengerMapper.java
@@ -0,0 +1,30 @@
+package com.java.train.mapper;
+
+import com.java.train.domain.Passenger;
+import com.java.train.domain.PassengerExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface PassengerMapper {
+    long countByExample(PassengerExample example);
+
+    int deleteByExample(PassengerExample example);
+
+    int deleteByPrimaryKey(Long id);
+
+    int insert(Passenger record);
+
+    int insertSelective(Passenger record);
+
+    List<Passenger> selectByExample(PassengerExample example);
+
+    Passenger selectByPrimaryKey(Long id);
+
+    int updateByExampleSelective(@Param("record") Passenger record, @Param("example") PassengerExample example);
+
+    int updateByExample(@Param("record") Passenger record, @Param("example") PassengerExample example);
+
+    int updateByPrimaryKeySelective(Passenger record);
+
+    int updateByPrimaryKey(Passenger record);
+}
