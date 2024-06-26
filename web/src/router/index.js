718426c62d39a1ac915824251736aa485089de3e
sw961174314 <961174314@qq.com>
2024-06-26 15:58:47 +0800

    2024.6.26 乘车人新增界面开发

--- web/src/router/index.js
+++ web/src/router/index.js
@@ -12,7 +12,14 @@ const routes = [
     component: () => import('../views/main.vue'),
     meta: {
       loginRequire: true
-    }
+    },
+    children: [{
+      path: 'welcome',
+      component: () => import('../views/main/welcome.vue')
+    },{
+      path: 'passenger',
+      component: () => import('../views/main/passenger')
+    }]
   }
 ]
 
