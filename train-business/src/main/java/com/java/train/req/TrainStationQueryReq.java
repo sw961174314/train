b16ed2bf859f509c484c55f5a2a97028b0392571
sw961174314 <961174314@qq.com>
2024-07-03 10:37:47 +0800

    2024.7.3 为火车车站页面增加车次查询条件

--- train-business/src/main/java/com/java/train/req/TrainStationQueryReq.java
+++ train-business/src/main/java/com/java/train/req/TrainStationQueryReq.java
@@ -1,12 +1,21 @@
 package com.java.train.req;
 
-import com.java.train.req.PageReq;
-
 public class TrainStationQueryReq extends PageReq {
+
+    private String trainCode;
+
+    public String getTrainCode() {
+        return trainCode;
+    }
+
+    public void setTrainCode(String trainCode) {
+        this.trainCode = trainCode;
+    }
 
     @Override
     public String toString() {
         return "TrainStationQueryReq{" +
+                "trainCode='" + trainCode + '\'' +
                 "} " + super.toString();
     }
 }
