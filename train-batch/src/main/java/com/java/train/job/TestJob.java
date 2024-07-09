6ffe2a095339e157dc20142b2bab2cf764fcbdd4
sw961174314 <961174314@qq.com>
2024-07-09 14:22:34 +0800

    2024.7.9 禁止调度任务的并发执行

--- train-batch/src/main/java/com/java/train/job/TestJob.java
+++ train-batch/src/main/java/com/java/train/job/TestJob.java
@@ -1,9 +1,12 @@
 package com.java.train.job;
 
+import org.quartz.DisallowConcurrentExecution;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
 
+// 禁止跑批并发执行
+@DisallowConcurrentExecution
 public class TestJob implements Job {
 
     @Override
