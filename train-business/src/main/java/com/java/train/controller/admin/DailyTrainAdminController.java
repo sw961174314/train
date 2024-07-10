67181fea614498190c435eb7fc7c77a606151ca2
sw961174314 <961174314@qq.com>
2024-07-10 11:46:35 +0800

    2024.7.10 快速生成每日车次数据管理功能

---
+++ train-business/src/main/java/com/java/train/controller/admin/DailyTrainAdminController.java
@@ -0,0 +1,38 @@
+package com.java.train.controller.admin;
+
+import com.java.train.context.LoginMemberContext;
+import com.java.train.resp.CommonResp;
+import com.java.train.resp.PageResp;
+import com.java.train.req.DailyTrainQueryReq;
+import com.java.train.req.DailyTrainSaveReq;
+import com.java.train.resp.DailyTrainQueryResp;
+import com.java.train.service.DailyTrainService;
+import jakarta.annotation.Resource;
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/admin/daily-train")
+public class DailyTrainAdminController {
+
+    @Resource
+    private DailyTrainService dailyTrainService;
+
+    @PostMapping("/save")
+    public CommonResp<Object> save(@Valid @RequestBody DailyTrainSaveReq req) {
+        dailyTrainService.save(req);
+        return new CommonResp<>();
+    }
+
+    @GetMapping("/query-list")
+    public CommonResp<PageResp<DailyTrainQueryResp>> queryList(@Valid DailyTrainQueryReq req) {
+        PageResp<DailyTrainQueryResp> list = dailyTrainService.queryList(req);
+        return new CommonResp<>(list);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public CommonResp<Object> delete(@PathVariable Long id) {
+        dailyTrainService.delete(id);
+        return new CommonResp<>();
+    }
+}
