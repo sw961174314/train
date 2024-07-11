6954d43bbb1bfd701132e0f7b3d44e7857f9aa81
sw961174314 <961174314@qq.com>
2024-07-11 15:40:44 +0800

    2024.7.11 快速生成每日座位数据管理功能

---
+++ train-business/src/main/java/com/java/train/controller/admin/DailyTrainSeatAdminController.java
@@ -0,0 +1,38 @@
+package com.java.train.controller.admin;
+
+import com.java.train.context.LoginMemberContext;
+import com.java.train.resp.CommonResp;
+import com.java.train.resp.PageResp;
+import com.java.train.req.DailyTrainSeatQueryReq;
+import com.java.train.req.DailyTrainSeatSaveReq;
+import com.java.train.resp.DailyTrainSeatQueryResp;
+import com.java.train.service.DailyTrainSeatService;
+import jakarta.annotation.Resource;
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/admin/daily-train-seat")
+public class DailyTrainSeatAdminController {
+
+    @Resource
+    private DailyTrainSeatService dailyTrainSeatService;
+
+    @PostMapping("/save")
+    public CommonResp<Object> save(@Valid @RequestBody DailyTrainSeatSaveReq req) {
+        dailyTrainSeatService.save(req);
+        return new CommonResp<>();
+    }
+
+    @GetMapping("/query-list")
+    public CommonResp<PageResp<DailyTrainSeatQueryResp>> queryList(@Valid DailyTrainSeatQueryReq req) {
+        PageResp<DailyTrainSeatQueryResp> list = dailyTrainSeatService.queryList(req);
+        return new CommonResp<>(list);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public CommonResp<Object> delete(@PathVariable Long id) {
+        dailyTrainSeatService.delete(id);
+        return new CommonResp<>();
+    }
+}
---
+++ train-business/src/main/java/com/java/train/req/DailyTrainSeatQueryReq.java
@@ -0,0 +1,12 @@
+package com.java.train.req;
+
+import com.java.train.req.PageReq;
+
+public class DailyTrainSeatQueryReq extends PageReq {
+
+    @Override
+    public String toString() {
+        return "DailyTrainSeatQueryReq{" +
+                "} " + super.toString();
+    }
+}
