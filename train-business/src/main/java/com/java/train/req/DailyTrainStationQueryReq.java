79926074d707f24239b045acbc79f22bacfffbb4
sw961174314 <961174314@qq.com>
2024-07-11 11:49:22 +0800

    2024.7.11 完善每日车站页面，增加日期和车次查询条件

--- train-business/src/main/java/com/java/train/req/DailyTrainStationQueryReq.java
+++ train-business/src/main/java/com/java/train/req/DailyTrainStationQueryReq.java
@@ -1,12 +1,38 @@
 package com.java.train.req;
 
 import com.java.train.req.PageReq;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.util.Date;
 
 public class DailyTrainStationQueryReq extends PageReq {
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private Date date;
+
+    private String trainCode;
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getTrainCode() {
+        return trainCode;
+    }
+
+    public void setTrainCode(String trainCode) {
+        this.trainCode = trainCode;
+    }
 
     @Override
     public String toString() {
         return "DailyTrainStationQueryReq{" +
+                "date=" + date +
+                ", trainCode='" + trainCode + '\'' +
                 "} " + super.toString();
     }
 }
