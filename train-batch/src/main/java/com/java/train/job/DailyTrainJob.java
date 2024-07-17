e4268a7f22515b4dab8d294f722343bbbdf26d87
sw961174314 <961174314@qq.com>
2024-07-17 15:34:34 +0800

    2024.7.17 增加生成每日车次定时任务

---
+++ train-batch/src/main/java/com/java/train/job/DailyTrainJob.java
@@ -0,0 +1,25 @@
+package com.java.train.job;
+
+import cn.hutool.core.util.RandomUtil;
+import org.quartz.DisallowConcurrentExecution;
+import org.quartz.Job;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
+
+// 禁止跑批并发执行
+@DisallowConcurrentExecution
+public class DailyTrainJob implements Job {
+
+    private static final Logger LOG = LoggerFactory.getLogger(DailyTrainJob.class);
+
+    @Override
+    public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
+        // 增加日志流水号
+        MDC.put("LOG_ID", System.currentTimeMillis() + RandomUtil.randomString(3));
+        LOG.info("生成每日车次数据开始");
+        LOG.info("生成每日车次数据结束");
+    }
+}
