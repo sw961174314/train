f90a804efef7cb6680e52741c87788673aa92088
sw961174314 <961174314@qq.com>
2024-08-09 10:26:31 +0800

    2024.8.9 集成Nacos配置中心，实现动态刷新的功能

--- train-member/src/main/java/com/java/train/controller/TestController.java
+++ train-member/src/main/java/com/java/train/controller/TestController.java
@@ -1,13 +1,20 @@
 package com.java.train.controller;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+// Nacos配置实时刷新
+@RefreshScope
 public class TestController {
+
+    @Value("${test.nacos}")
+    private String testNacos;
 
     @GetMapping("/hello")
     public String hello() {
-        return "Hello world";
+        return "Hello " + testNacos;
     }
 }
