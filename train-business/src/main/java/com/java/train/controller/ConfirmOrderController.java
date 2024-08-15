6054b97c6b7d5dee753b41f09c3001a6d72cd63f
sw961174314 <961174314@qq.com>
2024-08-15 11:29:43 +0800

    2024.8.15 Sentinel+Nacos实现限流规则持久化

--- train-business/src/main/java/com/java/train/controller/ConfirmOrderController.java
+++ train-business/src/main/java/com/java/train/controller/ConfirmOrderController.java
@@ -1,22 +1,45 @@
 package com.java.train.controller;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
+import com.java.train.exception.BusinessException;
+import com.java.train.exception.BusinessExceptionEnum;
 import com.java.train.req.ConfirmOrderDoReq;
 import com.java.train.resp.CommonResp;
 import com.java.train.service.ConfirmOrderService;
 import jakarta.annotation.Resource;
 import jakarta.validation.Valid;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/confirm-order")
 public class ConfirmOrderController {
 
+    private static final Logger LOG = LoggerFactory.getLogger(ConfirmOrderService.class);
+
     @Resource
     private ConfirmOrderService confirmOrderService;
 
+    @SentinelResource(value = "confirmOrderDo",blockHandler = "doConfirmBlock")
     @PostMapping("/do")
     public CommonResp<Object> doConfirm(@Valid @RequestBody ConfirmOrderDoReq req) {
         confirmOrderService.doConfirm(req);
         return new CommonResp<>();
+    }
+
+    /**
+     * 降级方法，需要包含限流方法的所有参数和BlockException参数
+     * @param req
+     * @param e
+     */
+    public CommonResp<Object> doConfirmBlock(ConfirmOrderDoReq req, BlockException e) {
+        LOG.info("购票请求被限流: {}", req);
+//        throw new BusinessException(BusinessExceptionEnum.CONFIRM_ORDER_FLOW_EXCEPTION);
+        CommonResp<Object> commonResp = new CommonResp<>();
+        commonResp.setSuccess(false);
+        commonResp.setMessage(BusinessExceptionEnum.CONFIRM_ORDER_FLOW_EXCEPTION.getDesc());
+        return commonResp;
     }
 }
